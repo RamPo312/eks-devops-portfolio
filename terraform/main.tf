465ca3a89a4858d7ca73cd6ab57127a3f68d417f
Ubuntu <ubuntu@ip-172-31-10-11.us-east-2.compute.internal>
2026-04-03 07:44:03 +0000

    final clean setup with CI/CD, k8s, terraform

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+
+
+########################################
+# VPC MODULE
+########################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["us-east-1a", "us-east-1b"]
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+########################################
+# EKS MODULE
+########################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.31"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  # ✅ FIX: Make API public
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
+  ########################################
+  # NODE GROUP
+  ########################################
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 2
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+
+    default = "us-east-1"
+
+}
+
+variable "cluster_name" {
+
+  default = "eks-devops-cluster"
+
+}
+
+
